85f597716f928c25b476fa060364301e430f1d8e
ashishS1709 <ashishs2360@gmail.com>
2025-03-05 14:24:43 +0530

    added the requirements on the files

--- main.py
+++ main.py
@@ -154,9 +154,4 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=443)
-    
-    
-    
-    
-    
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
